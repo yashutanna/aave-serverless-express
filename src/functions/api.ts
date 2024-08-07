d9ba46bbec968524f4c7678a2f42de34c96bb09a
yashu <6696871+yashutanna@users.noreply.github.com>
2024-08-07 21:08:44 +0200

    update route

--- src/functions/api.ts
+++ src/functions/api.ts
@@ -6,6 +6,6 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use('/.netlify/functions/api', router); // Path must match the redirect in netlify.toml
+api.use('/netlify/functions/api', router); // Path must match the redirect in netlify.toml
 
 export const handler = serverless(api as Application);
