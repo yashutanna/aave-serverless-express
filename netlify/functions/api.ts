b8c32c59158564f6e1acdd166bf7d0d1dbed0a84
yashu <6696871+yashutanna@users.noreply.github.com>
2024-08-07 21:04:14 +0200

    configure netlify rewrite path to express app

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,11 +1,11 @@
 import express, { Router } from "express";
-import serverless from "serverless-http";
+import serverless, { Application } from "serverless-http";
 
 const api = express();
 
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
+api.use('/.netlify/functions/api', router); // Path must match the redirect in netlify.toml
 
-export const handler = serverless(api);
+export const handler = serverless(api as Application);
